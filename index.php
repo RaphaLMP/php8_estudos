fa438874ce1c3e8fccc82e3472f3bcca270391cf
Raphael Martins <moltrescode@gmail.com>
2024-11-20 15:10:43 -0300

    Apredendo sobre Tomada de Decisão IF, Else e Else IF

--- index.php
+++ index.php
@@ -3,13 +3,23 @@
 // Aqui são as variáveis
 $nome = "Raphael";
 $sobrenome = "Martins";
-$idade = 23;
+$idade = 24;
 $numero = 10;
+$mediaIdade = 30;
 
 // echo $idade ."<br>";
-echo $nome ." ". $sobrenome . " e a sua idade é " .$idade;
+echo $nome . " " . $sobrenome . " e a sua idade é " . $idade;
 
 $total = $idade + $numero;
-echo "<p>" .$total;
+echo "<p>" . $total;
+
+// Tomadas de decisão IF
+if ($idade > $mediaIdade) {
+    echo "<p> Idade maior que " . $mediaIdade;
+}else if($idade == 23){
+    echo "<p> Idade igual a 23 anos";
+}else {
+    echo "<p> Idade não é maior que " . $mediaIdade . " e também não é igual a 23 anos";
+}
 
 ?>
