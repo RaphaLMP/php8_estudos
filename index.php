32d055a3706642cca0a4887ac469d5de3feca28c
Raphael Martins <moltrescode@gmail.com>
2024-11-20 14:51:01 -0300

    Aprendendo sobre variáveis PHP

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php
+
+// Aqui são as variáveis
+$nome = "Raphael";
+$sobrenome = "Martins";
+$idade = 23;
+$numero = 10;
+
+// echo $idade ."<br>";
+echo $nome ." ". $sobrenome . " e a sua idade é " .$idade;
+
+$total = $idade + $numero;
+echo "<p>" .$total;
+
+?>
